61f0d592fd83f3e6645ee1a28f4e321c8e48170e
Mahmood Ramezani <m10241@gmail.com>
2024-01-01 18:28:20 -0500

    refactors topnav items (#5)

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -1,14 +1,7 @@
 'use client'
-import Script from "next/script";
 
 export default function Lab() {
     return (
-        <>
-            <h1>Hello!</h1>
-            <Script id='open-cv-script' src="https://docs.opencv.org/5.x/opencv.js" onReady={() => {
-                console.log('open cv is ready!')
-            }} />
-
-        </>
+        <h1>Hello!</h1>
     )
 }
